932c565e24fe974caaae4d1426e80d4a9780b37a
pacotegg <francisco.gamiz@gmail.com>
2026-07-03 22:04:25 +0200

    Refactor browser into modular views

--- Modules/UI/Browser.psm1
+++ Modules/UI/Browser.psm1
@@ -7,73 +7,116 @@ function Show-SCMBrowseMenu {
         [array]$Games
     )
 
+    do {
+
+        Clear-Host
+
+        Write-Host ""
+        Write-Host "========== COLLECTION ==========" -ForegroundColor Cyan
+        Write-Host ""
+
+        Write-Host "1  Browse by Title"
+        Write-Host "2  Browse by Series"
+        Write-Host "3  Browse by Engine"
+        Write-Host "4  Browse by Language"
+        Write-Host "5  Browse by Platform"
+        Write-Host "0  Back"
+
+        Write-Host ""
+
+        $Option = Read-Host "Option"
+
+        switch ($Option) {
+
+            "1" {
+                Show-SCMBrowseByTitle -Games $Games
+            }
+
+            "2" {
+                Show-SCMBrowseBySeries -Games $Games
+            }
+
+            "3" {
+                Write-Host ""
+                Write-Host "Not implemented yet." -ForegroundColor Yellow
+                Pause-SCM
+            }
+
+            "4" {
+                Write-Host ""
+                Write-Host "Not implemented yet." -ForegroundColor Yellow
+                Pause-SCM
+            }
+
+            "5" {
+                Write-Host ""
+                Write-Host "Not implemented yet." -ForegroundColor Yellow
+                Pause-SCM
+            }
+
+        }
+
+    } until ($Option -eq "0")
+}
+
+function Show-SCMBrowseByTitle {
+
+    param(
+        [Parameter(Mandatory)]
+        [array]$Games
+    )
+
     Clear-Host
 
     Write-Host ""
-    Write-Host "========== COLLECTION ==========" -ForegroundColor Cyan
+    Write-Host "========== BY TITLE ==========" -ForegroundColor Cyan
     Write-Host ""
 
-    Write-Host "1  Browse by Title"
-    Write-Host "2  Browse by Series"
-    Write-Host "3  Browse by Engine"
-    Write-Host "4  Browse by Language"
-    Write-Host "5  Browse by Platform"
-    Write-Host "0  Back"
+    $Index = 1
+
+    foreach ($Game in ($Games | Sort-Object Title)) {
+
+        Write-Host ("{0,3}. {1}" -f $Index, $Game.Title)
+
+        $Index++
+    }
 
     Write-Host ""
-
-    $option = Read-Host "Option"
-
-    switch ($option) {
-
-    "1" {
-
-        foreach ($Game in ($Games | Sort-Object Title)) {
-            Write-Host ("{0,-20} {1}" -f $Game.ShortID, $Game.Title)
-        }
-
-        Pause-SCM
-    }
-
-    "2" {
-
-        $Series = $Games |
-            Where-Object { $_.Series } |
-            Group-Object Series |
-            Sort-Object Name
-
-        foreach ($Group in $Series) {
-
-            Write-Host ""
-            Write-Host $Group.Name -ForegroundColor Cyan
-
-            foreach ($Game in ($Group.Group | Sort-Object Title)) {
-                Write-Host ("    {0}" -f $Game.Title)
-            }
-        }
-
-        Pause-SCM
-    }
-
-    "3" {
-        Write-Host "Not implemented yet."
-        Pause-SCM
-    }
-
-    "4" {
-        Write-Host "Not implemented yet."
-        Pause-SCM
-    }
-
-     "5" {
-        Write-Host "Not implemented yet."
-        Pause-SCM
-    }
-
-    "0" { }
-
+    Pause-SCM
 }
 
+function Show-SCMBrowseBySeries {
+
+    param(
+        [Parameter(Mandatory)]
+        [array]$Games
+    )
+
+    Clear-Host
+
+    Write-Host ""
+    Write-Host "========== BY SERIES ==========" -ForegroundColor Cyan
+    Write-Host ""
+
+    $Series = $Games |
+        Where-Object { $_.Series } |
+        Group-Object Series |
+        Sort-Object Name
+
+    foreach ($Group in $Series) {
+
+        Write-Host ""
+        Write-Host $Group.Name -ForegroundColor Cyan
+
+        foreach ($Game in ($Group.Group | Sort-Object Title)) {
+
+            Write-Host ("    {0}" -f $Game.Title)
+
+        }
+    }
+
+    Write-Host ""
+    Pause-SCM
 }
 
 Export-ModuleMember -Function Show-SCMBrowseMenu
